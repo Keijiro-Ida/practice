f3d52c94b66ded0e84ff2b38c99b9599695e4c68
Keijiro_Ida <idatt1122@gmail.com>
2025-02-20 06:18:54 +0900

    20250220

---
+++ isSymmetric.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        queue = []
+
+        def compare(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            if t1.val != t2.val:
+                return False
+            return compare(t1.left, t2.right) and compare(t1.right, t2.left)
+
+        return compare(root.left, root.right)
