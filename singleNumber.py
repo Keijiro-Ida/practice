b607e7ea7f2073aa7443fe4d4fce2a1e35d82af4
Keijiro_Ida <idatt1122@gmail.com>
2025-03-02 07:16:21 +0900

    20250302

---
+++ singleNumber.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+
+        for num in nums:
+            result ^= num
+
+        return resultclass Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+
+        for num in nums:
+            result ^= num
+
+        return result
